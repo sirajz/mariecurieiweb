03b1e0c1864d3e48b06a7eb4108aabf1eef1f46a
Zarook <Siraj.Zarook@aecom.com>
2017-06-27 14:41:44 +0100

    code project

---
+++ MarieCurie.HelperServices.Utilities/IHelperServicesUtil.cs
@@ -0,0 +1,16 @@
+﻿using MarieCurie.Interview.Assets.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarieCurie.HelperServices.Utilities
+{
+    public interface IHelperServicesUtil
+    {
+        bool checkOpenningHours(string dayOfWeek, int Hour, HelperService center);
+        string nextOpenningHours(string dayOfWeek, int Hour, HelperService center);
+
+    }
+}
